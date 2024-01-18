ce61344275a17ef466b4fc3861dec48da58c8e17
Rudra5266 <157023342+Rudra5266@users.noreply.github.com>
2024-01-18 23:09:47 +0530

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,6 +5,6 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-add_lunch_combo omni_1901-user
-add_lunch_combo omni_1901-userdebug
-add_lunch_combo omni_1901-eng
+add_lunch_combo twrp_1901-user
+add_lunch_combo twrp_1901-userdebug
+add_lunch_combo twrp_1901-eng
